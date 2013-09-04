ecfd84730e061aac6fd263c1df2fe44bf14a6930
murmon <livyy1@gmail.com>
2013-09-04 23:33:37 +0300

    chat added

--- php/themes/bootstrap/views/layouts/main.php
+++ php/themes/bootstrap/views/layouts/main.php
@@ -20,7 +20,6 @@
             'class'=>'bootstrap.widgets.TbMenu',
             'items'=>array(
                 array('label'=>'Home', 'url'=>array('/site/index')),
-                array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
                 array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
             ),
         ),
